ed81872c3347fa30bf84c5087d882cd4fe348759
uyenhoang9034-pixel <uyenhoang9034@gmail.com>
2026-08-28 23:20:42 +0700

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -58,8 +58,6 @@ export default {
       try {
         InteractionHelper.patchInteractionResponses(interaction);
         ResponseCoordinator.attach(interaction);
-        InteractionHelper.patchInteractionResponses(interaction);
-ResponseCoordinator.attach(interaction);
 
 // Giveaway dashboard components
 if (
