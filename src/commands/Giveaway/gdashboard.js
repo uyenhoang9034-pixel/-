979dfc12a6f1a426ae9281cdd6d8bd2ac04e885a
uyenhoang9034-pixel <uyenhoang9034@gmail.com>
2026-08-28 21:41:24 +0700

    Implement giveaway dashboard command

--- src/commands/Giveaway/gdashboard.js
+++ src/commands/Giveaway/gdashboard.js
@@ -1,1 +1,55 @@
-/gdashboard
+import {
+    SlashCommandBuilder,
+    PermissionFlagsBits,
+    MessageFlags,
+} from 'discord.js';
+
+import { TitanBotError, ErrorTypes } from '../../utils/errorHandler.js';
+import { InteractionHelper } from '../../utils/interactionHelper.js';
+import giveawayDashboard from './modules/giveaway_dashboard.js';
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('gdashboard')
+        .setDescription('Open the giveaway dashboard.')
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+
+    async execute(interaction) {
+        if (!interaction.inGuild()) {
+            throw new TitanBotError(
+                'Giveaway dashboard used outside guild',
+                ErrorTypes.VALIDATION,
+                'This command can only be used in a server.',
+                {
+                    userId: interaction.user.id,
+                },
+            );
+        }
+
+        if (
+            !interaction.member.permissions.has(
+                PermissionFlagsBits.ManageGuild,
+            )
+        ) {
+            throw new TitanBotError(
+                'User lacks ManageGuild permission',
+                ErrorTypes.PERMISSION,
+                "You need the 'Manage Server' permission to use the giveaway dashboard.",
+                {
+                    userId: interaction.user.id,
+                    guildId: interaction.guildId,
+                },
+            );
+        }
+
+        await InteractionHelper.safeDefer(interaction, {
+            flags: MessageFlags.Ephemeral,
+        });
+
+        return giveawayDashboard.execute(
+            interaction,
+            null,
+            interaction.client,
+        );
+    },
+};
