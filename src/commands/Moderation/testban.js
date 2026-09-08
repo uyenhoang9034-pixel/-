437efabe088183e2c6579a9fcec522c44c7b0a33
Em Chanh <uyenhoang9034@gmail.com>
2026-09-08 21:54:21 +0700

    Update testban.js

--- src/commands/Moderation/testban.js
+++ src/commands/Moderation/testban.js
@@ -2,17 +2,8 @@ import {
     SlashCommandBuilder,
     PermissionFlagsBits,
     EmbedBuilder,
+    MessageFlags,
 } from 'discord.js';
-
-import {
-    InteractionHelper,
-} from '../../utils/interactionHelper.js';
-
-/**
- * =========================================================
- * CONFIG
- * =========================================================
- */
 
 const MODERATION_CHANNEL_ID =
     '1546893787123556404';
@@ -21,10 +12,10 @@ const EMOJIS = {
     decoration:
         '<a:bang3:1546891744237461635>',
 
-    banTitle:
+    title:
         '<a:bang1:1546891405371117668>',
 
-    ban:
+    item:
         '<:ban1:1546891613261922377>',
 
     reasonEnd:
@@ -34,44 +25,34 @@ const EMOJIS = {
 export default {
     data:
         new SlashCommandBuilder()
-            .setName(
-                'testban',
-            )
+            .setName('testban')
             .setDescription(
-                'Test giao diện thông báo Ban',
+                'Xem thử giao diện thông báo Ban',
             )
             .addUserOption(
-                (option) =>
+                option =>
                     option
-                        .setName(
-                            'target',
-                        )
+                        .setName('target')
                         .setDescription(
-                            'Thành viên hiển thị trong bản test',
+                            'Thành viên dùng để hiển thị thử',
                         )
-                        .setRequired(
-                            true,
-                        ),
+                        .setRequired(true),
             )
             .addStringOption(
-                (option) =>
+                option =>
                     option
-                        .setName(
-                            'reason',
-                        )
+                        .setName('reason')
                         .setDescription(
-                            'Lý do hiển thị trong bản test',
+                            'Lý do hiển thị thử',
                         )
-                        .setRequired(
-                            true,
-                        ),
+                        .setRequired(true),
             )
             .setDefaultMemberPermissions(
                 PermissionFlagsBits.BanMembers,
             ),
 
     category:
-        'moderation',
+        'Moderation',
 
     async execute(
         interaction,
@@ -79,93 +60,87 @@ export default {
         client,
     ) {
         const target =
-            interaction.options
-                .getUser(
-                    'target',
-                    true,
-                );
+            interaction.options.getUser(
+                'target',
+                true,
+            );
 
         const reason =
-            interaction.options
-                .getString(
-                    'reason',
-                    true,
-                );
+            interaction.options.getString(
+                'reason',
+                true,
+            );
 
         const channel =
-            await interaction.guild
-                .channels
+            await interaction.guild.channels
                 .fetch(
                     MODERATION_CHANNEL_ID,
                 )
-                .catch(
-                    () => null,
-                );
+                .catch(() => null);
 
         if (
             !channel ||
             !channel.isTextBased()
         ) {
-            await InteractionHelper
-                .universalReply(
-                    interaction,
-                    {
-                        content:
-                            'Không tìm thấy kênh moderation.',
-                        ephemeral:
-                            true,
-                    },
-                );
+            await interaction.reply({
+                content:
+                    `Không tìm thấy kênh <#${MODERATION_CHANNEL_ID}>.`,
+                flags:
+                    MessageFlags.Ephemeral,
+            });
 
             return;
         }
 
+        /**
+         * TEST ONLY.
+         *
+         * KHÔNG ban.
+         * KHÔNG tạo moderation case.
+         * KHÔNG ghi database.
+         */
+
+        const fakeCaseId =
+            'TEST-001';
+
         const embed =
             new EmbedBuilder()
-                .setColor(
-                    0xf29ab2,
-                )
+                .setColor(0xf29ab2)
                 .setTitle(
                     `${EMOJIS.decoration} 𝓤𝓼𝓪𝓰𝓲 𝓜𝓸𝓭𝓮𝓻𝓪𝓽𝓲𝓸𝓷 ${EMOJIS.decoration}`,
                 )
                 .setDescription(
                     [
-                        `${EMOJIS.banTitle} **Thành viên đã bị BAN**`,
+                        `${EMOJIS.title} **Thành viên đã bị BAN**`,
 
                         '',
-                        `${EMOJIS.ban} **Thành viên**`,
+                        `${EMOJIS.item} **Thành viên**`,
                         `<@${target.id}>`,
 
                         '',
-                        `${EMOJIS.ban} **Người xử lý**`,
+                        `${EMOJIS.item} **Người xử lý**`,
                         `<@${interaction.user.id}>`,
 
                         '',
-                        `${EMOJIS.ban} **Lý do**`,
+                        `${EMOJIS.item} **Lý do**`,
                         `Vi phạm nội quy: ${reason} ${EMOJIS.reasonEnd}`,
 
                         '',
-                        `${EMOJIS.ban} **Case**`,
-                        '#TEST-001',
+                        `${EMOJIS.item} **Case**`,
+                        `#${fakeCaseId}`,
                     ].join('\n'),
                 )
                 .setTimestamp();
 
         await channel.send({
-            embeds: [
-                embed,
-            ],
+            embeds: [embed],
         });
 
-        await InteractionHelper
-            .universalReply(
-                interaction,
-                {
-                    content:
-                        `Đã gửi bản test Ban vào <#${MODERATION_CHANNEL_ID}>. Không có thành viên nào bị ban.`,
-                    ephemeral:
-                        true,
-                },
-            );
+        await interaction.reply({
+            content:
+                `Đã gửi giao diện test Ban vào <#${MODERATION_CHANNEL_ID}>. Không ai bị ban.`,
+            flags:
+                MessageFlags.Ephemeral,
+        });
     },
 };
