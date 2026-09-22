df72df6fd6ea6644ff424b53e9bc08c9f8a27aba
Em Chanh <uyenhoang9034@gmail.com>
2026-09-22 10:20:49 +0700

    Remove aliases for deleted Utility commands

--- src/config/commands/commandAliases.js
+++ src/config/commands/commandAliases.js
@@ -85,8 +85,6 @@ export const commandAliases = {
 
     'calc': 'calculate',
     'math': 'calculate',
-    'weather': 'weather',
-    'todo': 'todo',
     'report': 'report',
     'userinfo': 'userinfo',
     'whois': 'userinfo',
