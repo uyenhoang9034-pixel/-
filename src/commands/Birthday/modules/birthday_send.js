264395d544fc708944eb4847d6afeaca573ae422
Em Chanh <uyenhoang9034@gmail.com>
2026-09-24 11:42:09 +0700

    fix: render manual birthday mention and calculated age

--- src/commands/Birthday/modules/birthday_send.js
+++ src/commands/Birthday/modules/birthday_send.js
@@ -24,13 +24,13 @@ function buildBirthdayEmbed(member, birthday) {
 
     return new EmbedBuilder()
         .setColor(0xFCEEC9)
-        .setTitle(`<a:trangtrig2:1546040703375904801> 𝓗𝓪𝓹𝓹𝔂 𝓑𝓲𝓻𝓽𝓱𝓭𝓪𝔂 ${member.toString()}! <a:trangtrig3:1546040818261954610>`)
         .setDescription(
+            `## <a:trangtrig2:1546040703375904801> 𝓗𝓪𝓹𝓹𝔂 𝓑𝓲𝓻𝓽𝓱𝓭𝓪𝔂 ${member.toString()}! <a:trangtrig3:1546040818261954610>\n\n` +
             `<a:giftg1:1543150714732412948> Chúc mừng sinh nhật ${member.toString()}!\n` +
             `<a:giftg1:1543150714732412948> **生日快乐** ${member.toString()}!\n` +
             `<a:giftg1:1543150714732412948> **お誕生日おめでとう** ${member.toString()}!\n` +
             `<a:giftg1:1543150714732412948> **생일 축하해** ${member.toString()}!\n\n` +
-            `*Ngày ${birthday.day} tháng ${birthday.month}*\n\n` +
+            `*Ngày ${birthday.day} tháng ${birthday.month}${birthday.year ? ` - ${new Date().getFullYear() - birthday.year} tuổi` : ''}*\n\n` +
             'Chúc bạn có một ngày sinh nhật thật vui vẻ và tuyệt vời! Tuổi mới lúc nào cũng mạnh khỏe, vui vẻ và thành công trong mọi lĩnh vực nhé!'
         )
         .setImage('https://raw.githubusercontent.com/uyenhoang9034-pixel/-/main/assets/birthday/birthday.jpg')
