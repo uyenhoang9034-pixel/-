9d8d29bb298a96264dff94ef0cfae35ced41d8af
Em Chanh <uyenhoang9034@gmail.com>
2026-09-24 12:05:34 +0700

    fix: restrict birthday remove to admin role

--- src/commands/Birthday/modules/birthday_remove.js
+++ src/commands/Birthday/modules/birthday_remove.js
@@ -1,7 +1,9 @@
-import { EmbedBuilder, PermissionFlagsBits, MessageFlags } from 'discord.js';
+import { EmbedBuilder, MessageFlags } from 'discord.js';
 import { deleteBirthday } from '../../../services/birthdayService.js';
 
 import { InteractionHelper } from '../../../utils/interactionHelper.js';
+
+const BIRTHDAY_ADMIN_ROLE_ID = '1545305594712432640';
 export default {
     async execute(interaction, config, client) {
         await InteractionHelper.safeDefer(interaction, { flags: MessageFlags.Ephemeral });
@@ -10,10 +12,10 @@ export default {
         const userId = targetUser.id;
         const guildId = interaction.guildId;
 
-        if (targetUser.id !== interaction.user.id && !interaction.memberPermissions?.has(PermissionFlagsBits.ManageGuild)) {
+        if (!interaction.member?.roles?.cache?.has(BIRTHDAY_ADMIN_ROLE_ID)) {
             const embed = new EmbedBuilder()
                 .setColor(0xFCEEC9)
-                .setDescription('Bạn cần quyền **Manage Server** để xóa sinh nhật của người khác.');
+                .setDescription('Bạn không có quyền sử dụng lệnh này.');
             return await InteractionHelper.safeEditReply(interaction, { embeds: [embed] });
         }
 
