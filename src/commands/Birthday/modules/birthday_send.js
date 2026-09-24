4b189974c8654a4d3066a6d1da2bd36166cc3fb0
Em Chanh <uyenhoang9034@gmail.com>
2026-09-24 11:42:44 +0700

    style: remove user mention from birthday heading

--- src/commands/Birthday/modules/birthday_send.js
+++ src/commands/Birthday/modules/birthday_send.js
@@ -25,7 +25,7 @@ function buildBirthdayEmbed(member, birthday) {
     return new EmbedBuilder()
         .setColor(0xFCEEC9)
         .setDescription(
-            `## <a:trangtrig2:1546040703375904801> 𝓗𝓪𝓹𝓹𝔂 𝓑𝓲𝓻𝓽𝓱𝓭𝓪𝔂 ${member.toString()}! <a:trangtrig3:1546040818261954610>\n\n` +
+            `## <a:trangtrig2:1546040703375904801> 𝓗𝓪𝓹𝓹𝔂 𝓑𝓲𝓻𝓽𝓱𝓭𝓪𝔂! <a:trangtrig3:1546040818261954610>\n\n` +
             `<a:giftg1:1543150714732412948> Chúc mừng sinh nhật ${member.toString()}!\n` +
             `<a:giftg1:1543150714732412948> **生日快乐** ${member.toString()}!\n` +
             `<a:giftg1:1543150714732412948> **お誕生日おめでとう** ${member.toString()}!\n` +
